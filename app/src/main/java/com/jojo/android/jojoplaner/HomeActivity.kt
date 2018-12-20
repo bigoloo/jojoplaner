a773e2d6bc24556be2738964cbfc1a3f432b8fdd
Amin Bahiraei <mr.bahiraee@gmail.com>
2018-12-21 01:18:41 +0330

    Create navigation controller for actions

--- app/src/main/java/com/jojo/android/jojoplaner/HomeActivity.kt
+++ app/src/main/java/com/jojo/android/jojoplaner/HomeActivity.kt
@@ -1,12 +1,13 @@
 package com.jojo.android.jojoplaner
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
-
+import android.view.View.GONE
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment.findNavController
 import kotlinx.android.synthetic.main.activity_home.*
+
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,8 +16,8 @@ class HomeActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            findNavController(nav_host_fragment).navigate(R.id.action_homeActivityFragment_to_addPlanFragment)
+            view.visibility=GONE
         }
     }
 
@@ -30,7 +31,7 @@ class HomeActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when(item.itemId) {
+        return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
