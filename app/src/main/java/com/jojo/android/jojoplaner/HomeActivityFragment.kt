978314cbae233c60b3f6f3259d0559a0aab0f22f
Amin Bahiraei <mr.bahiraee@gmail.com>
2018-12-20 20:10:55 +0330

    The fist commit of Jojo Planner

---
+++ app/src/main/java/com/jojo/android/jojoplaner/HomeActivityFragment.kt
@@ -0,0 +1,18 @@
+package com.jojo.android.jojoplaner
+
+import android.support.v4.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+class HomeActivityFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
